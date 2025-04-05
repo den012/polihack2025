0bb72964682707ecbd2e12bbb06c39cb38d5279a
Denis <blagadeniss@gmail.com>
2025-04-05 21:18:26 +0300

    changed vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": "https://2872-5-2-197-133.ngrok-free.app",
     }
   },
   plugins: [
