ea1cd3ee7606bbd4fee0f3ea84e435eacc51cf93
Denis <blagadeniss@gmail.com>
2025-04-06 10:09:10 +0300

    updated api url in vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://2872-5-2-197-133.ngrok-free.app",
+      "/api": process.env.VITE_API_URL || "",
     }
   },
   plugins: [
